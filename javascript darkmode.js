7f4a8a56b8d89f2bb181967df45c692249fedc3c
XavierCruces98 <vier_98@hotmail.com>
2023-05-25 12:44:01 -0500

    proyector encriptador 25-05-2023-12.43pm

---
+++ javascript darkmode.js
@@ -0,0 +1,97 @@
+"use strict"
+
+let change= true
+const HTML_contenedor= document.querySelector(".contenedor")
+const HTML_contenedor_traslado= document.querySelector(".contenedor-traslado")
+const HTML_imagenDarkMode= document.querySelector(".imagen-dark-mode")
+const HTML_hoja_css = document.querySelector("#mi-hoja-css")
+
+const HTML_imagen_logo= document.querySelector(".imagen-logo")
+const HTML_imagen_vector= document.querySelector(".imagen-vector")
+
+HTML_imagenDarkMode.addEventListener("click", (evento)=>{ 
+  evento.preventDefault(); 
+  const evento_local = evento.target
+  console.log(`TESTING img`, evento_local);
+
+  cambiar_boton() ;
+  evento.stopPropagation(); //evita que se duplique el evento
+})
+
+HTML_contenedor.addEventListener("click", (evento)=>{ 
+
+  evento.preventDefault(); 
+  const evento_local = evento.target
+  console.log(`TESTING contenedor`, evento_local);
+
+  cambiar_boton() 
+})
+
+
+function cambiar_boton() {
+  if(change){
+    HTML_hoja_css.href="estilos-dark.css",
+
+    HTML_imagenDarkMode.classList.remove("imagen-desopacar")  // desopacado
+    HTML_contenedor.classList.add("contenedor-black") // !Contenedor => oscurecemos  
+    HTML_contenedor_traslado.classList.add("trasladar-sol-luna") //trasladamos y rotamos
+    HTML_imagenDarkMode.classList.add("imagen-opacar") // primero opacamos
+  
+    setTimeout(() => { // tiempo para que termine de rotar
+
+      HTML_contenedor.classList.remove("contenedor") // 
+      HTML_contenedor.classList.add("contenedor-2") // !Contenedor => oscurecemos  
+
+      HTML_contenedor_traslado.classList.remove("sol"), // despues de trasladar, cambiamos formato
+      HTML_contenedor_traslado.classList.add("luna") , // despues de trasladar, cambiamos formato
+      
+      HTML_imagenDarkMode.classList.remove("imagen-opacar")   //  primero opacamos, quitamos esta clase
+      HTML_imagenDarkMode.classList.add("imagen-desopacar") // segundo despopacamos
+   
+      HTML_imagenDarkMode.setAttribute("src","imagenes/luna.png" ) ;
+  
+
+      HTML_contenedor.classList.remove("contenedor-black") // !Contenedor => oscurecido
+      HTML_contenedor_traslado.classList.remove("trasladar-sol-luna") // trasladado
+
+    }, 500);
+
+    HTML_imagen_logo.setAttribute("src","imagenes/Logo-blanco.png" ) 
+    HTML_imagen_vector.setAttribute("src","imagenes/vector-blanco.png" ) 
+
+    change=!change;
+  }
+
+  else {
+    HTML_hoja_css.href= "estilos-ligth.css";
+
+    HTML_imagenDarkMode.classList.remove("imagen-desopacar")   // desopacado
+    HTML_contenedor.classList.add("contenedor-white") // !Contenedor => aclaramos  
+    HTML_contenedor_traslado.classList.add("trasladar-luna-sol") //trasladamos
+    HTML_imagenDarkMode.classList.add("imagen-opacar") // primero opacamos
+  
+    setTimeout(() => {
+      HTML_contenedor.classList.remove("contenedor-2") // 
+      HTML_contenedor.classList.add("contenedor") // !Contenedor => aclaramos 
+  
+      HTML_contenedor_traslado.classList.remove("luna"), // despues de trasladar, cambiamos formato
+      HTML_contenedor_traslado.classList.add("sol") , // despues de trasladar, cambiamos formato
+
+      HTML_imagenDarkMode.classList.remove("imagen-opacar") 
+      HTML_imagenDarkMode.classList.add("imagen-desopacar") 
+
+      HTML_imagenDarkMode.setAttribute("src","imagenes/sol.png" );
+
+      HTML_contenedor.classList.remove("contenedor-white") // !Contenedor => aclarado
+      HTML_contenedor_traslado.classList.remove("trasladar-luna-sol") // trasladado
+     
+    }, 500);
+
+    HTML_imagen_logo.setAttribute("src","imagenes/LogoRecortado.png" ) 
+    HTML_imagen_vector.setAttribute("src","imagenes/vector.png" )
+
+    change=!change;
+  }
+}
+
+
---
+++ javascript logica.js
@@ -0,0 +1,101 @@
+"use strict"
+const HTML_textArea = document.querySelector("#textarea-1")
+const HTML_botonEncriptar= document.querySelector(".btn-encriptar")
+const HTML_botonDesencriptar = document.querySelector(".btn-desencriptar")
+
+const HTML_mensaje = document.querySelector(".mostrar-texto")
+const regex = /^([\s?]*[a-z]+[\s?]*)+$/ 
+
+const evaluar_text = function(){
+  if( !regex.test(HTML_textArea.value) ){
+    HTML_mensaje.innerHTML=`<img src="imagenes/Muñeco.png" id="foto-2">
+    <h2>Ningún mensaje fue encontrado</h2>
+    <p>Ingresa el texto que desea encriptar o desencriptar.</p>`  
+
+    HTML_mensaje.classList.add("sin-resultado")
+    return true
+  }
+  else {
+    HTML_mensaje.classList.remove("sin-resultado")
+    return false
+  }
+  
+}
+
+///////////////////
+
+const crear_boton_copiar= function(){
+  const HTML_copiar = document.querySelector(".btn-copiar")
+  const  HTML_textoAcopiar = document.querySelector("#textarea-2").textContent;
+
+  HTML_copiar.addEventListener("click", function(){
+    // console.log(`Texto que recien vamos a copiar ===_`, HTML_textoAcopiar,"_");
+    const elemento_oculto = document.createElement("textarea"); //"textarea" nos permite copiar con todo y salto de linea
+    elemento_oculto.innerHTML= HTML_textoAcopiar
+    document.body.appendChild(elemento_oculto)
+    elemento_oculto.select()
+    document.execCommand("copy") //es normal que salga asi "--"
+    document.body.removeChild(elemento_oculto)
+  })
+}
+//////////////////
+
+HTML_botonEncriptar.addEventListener("click", ()=>{
+  if(evaluar_text()) return;
+ 
+  const texto_encriptado=[]
+  const texto = HTML_textArea.value
+
+  for(let variable of [...texto]){
+
+    if(variable==="e")      texto_encriptado.push("enter")
+    else if(variable==="i") texto_encriptado.push("imes")
+    else if(variable==="a") texto_encriptado.push("ai")
+    else if(variable==="o") texto_encriptado.push("ober")
+    else if(variable==="u") texto_encriptado.push("ufat")
+    else { texto_encriptado.push(variable)}
+  }
+
+  HTML_mensaje.innerHTML=`
+  <textarea class="informacion-encriptado" id="textarea-2" readonly>${texto_encriptado.join("")}</textarea>
+    <div class="btn-copiar-contenedor">
+      <button class="btn-copiar">Copiar</button>
+    </div>`
+
+  crear_boton_copiar()
+})
+
+HTML_botonDesencriptar.addEventListener("click", ()=>{
+  if(evaluar_text()) return;
+  const texto_desencriptado=[]
+  const texto = HTML_textArea.value
+  .replace(/enter/g, "1")
+  .replace(/imes/g, "2")
+  .replace(/ai/g, "3")
+  .replace(/ober/g, "4")
+  .replace(/ufat/g, "5")
+
+  for(let variable of [...texto]){
+
+    if(variable==="1")      texto_desencriptado.push("e")
+    else if(variable==="2") texto_desencriptado.push("i")
+    else if(variable==="3") texto_desencriptado.push("a")
+    else if(variable==="4") texto_desencriptado.push("o")    
+    else if(variable==="5") texto_desencriptado.push("u")
+    else { texto_desencriptado.push(variable)}
+  }
+
+  HTML_mensaje.innerHTML=`
+  <textarea class="informacion-encriptado" id="textarea-2" readonly>${texto_desencriptado.join("")}</textarea>
+    <div class="btn-copiar-contenedor">
+      <button class="btn-copiar">Copiar</button>
+    </div>`
+
+  crear_boton_copiar()
+
+})
+
+
+
+
+
